fe969169c8bb039348c9c6827c27547eb9f10fea
unknown <bianca.mares13@yahoo.com>
2020-03-19 18:07:48 +0200

    project structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {ProductsComponent} from './products/products.component';
+
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent},
+  {
+    path: '',
+    component: ProductsComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
